85924bdaba737b3d32dab83ac6ad4aea4d29c378
Alex Tan <AlexTAN331@outlook.com>
2020-06-14 11:38:42 -0400

    added routes to connect frontend for post and put

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -17,11 +17,11 @@ const seedDatabase = async () => {
       address: "Manhattan",
       description: "A college in Manhattan",
     }),
-    Student.create({ firstName: "Daniel", lastName: "Lee", email: "daniellee@example.com", gpa: 3.5, campusId: 1 }),
-    Student.create({ firstName: "John", lastName: "Doe", email: "johndoe@example.com", gpa: 3.2, campusId: 1 }),
-    Student.create({ firstName: "Tony", lastName: "Parker", email: "tonyparker@example.com", gpa: 3.6, campusId: 2 }),
-    Student.create({ firstName: "Megan", lastName: "White", email: "meganwhite@example.com", gpa: 3.6, campusId: 2 }),
-    Student.create({ firstName: "Carter", lastName: "James", email: "carterjames@example.com", gpa: 4.0, campusId: 3 }),
+    Student.create({ firstName: "Daniel", lastName: "Lee", email: "daniellee@example.com", gpa: 3.51, campusId: 1 }),
+    Student.create({ firstName: "John", lastName: "Doe", email: "johndoe@example.com", gpa: 3.29, campusId: 1 }),
+    Student.create({ firstName: "Tony", lastName: "Parker", email: "tonyparker@example.com", gpa: 3.60, campusId: 2 }),
+    Student.create({ firstName: "Megan", lastName: "White", email: "meganwhite@example.com", gpa: 3.69, campusId: 2 }),
+    Student.create({ firstName: "Carter", lastName: "James", email: "carterjames@example.com", gpa: 4.00, campusId: 3 }),
   ]);
 };
 
